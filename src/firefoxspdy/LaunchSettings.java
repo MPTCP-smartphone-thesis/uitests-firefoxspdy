abe016a25f98598aff3db3694450f4b70135ff7d
Matthieu Baerts <matttbe@gmail.com>
2014-11-25 14:43:03 +0100

    Init version: without the proxy

---
+++ src/firefoxspdy/LaunchSettings.java
@@ -0,0 +1,13 @@
+package firefoxspdy;
+
+import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+import common.FirefoxCommon;
+
+public class LaunchSettings extends UiAutomatorTestCase {
+
+	public void testDemo() throws UiObjectNotFoundException {
+		new FirefoxCommon(this, "firefoxspdy", true).testDemo();
+	}
+
+}
